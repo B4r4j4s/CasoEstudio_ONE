665b3c6f21ac1b1f14a3a0426abd36410b7893d8
B4r4jas <ulises.barajas1297@alumnos.udg.mx>
2023-10-12 23:41:19 -0600

    Implementacion de alert messages en nase.html
    Implemente alert para que sea posible mandar flashes al ususario y comunicarle mensajes con categorias, ver auth.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+
+#db = SQLAlchemy()
+#DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'qwerty'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # DATABASE LOCATION
 
     from .views import views #importar los files
     from .auth import auth
